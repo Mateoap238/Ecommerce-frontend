e84c0bc040ca5b1a66e30471685bf6cd66032c4f
malvarez <malvarez@ditransa.com.co>
2024-11-27 21:43:54 -0500

    fix: :bug: Inused imports

--- src/app/business/components/about-us/about-us.component.ts
+++ src/app/business/components/about-us/about-us.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css',
 })
